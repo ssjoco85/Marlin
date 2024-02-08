6e37dbb86b5716c08225bae5a281511a8c03286a
ssjoco85 <122283808+ssjoco85@users.noreply.github.com>
2024-02-08 12:26:49 +0100

    Update Configuration.h
    estep

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1237,7 +1237,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 842 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 888.1 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
