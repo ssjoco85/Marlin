2e2e0f751037185179cb5fa5f6b6e5300d9e35de
ssjoco85 <122283808+ssjoco85@users.noreply.github.com>
2025-01-20 23:13:14 +0100

    offsert

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1642,7 +1642,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, -44, -2.82}
+#define NOZZLE_TO_PROBE_OFFSET { -44, -4, -3.10}
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
