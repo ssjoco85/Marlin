488b94ed344acd254501a39128bfa4bece918d8a
ssjoco85 <122283808+ssjoco85@users.noreply.github.com>
2025-02-06 22:02:47 +0100

    accel2

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1308,7 +1308,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 500, 500, 20, 150 }
+#define DEFAULT_MAX_FEEDRATE          { 500, 500, 20, 50 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -1321,7 +1321,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 2500, 2500, 200, 8000 }
+#define DEFAULT_MAX_ACCELERATION      { 2500, 2500, 300, 8000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -1337,7 +1337,7 @@
  *   M204 T    Travel Acceleration
  */
 #define DEFAULT_ACCELERATION           1200    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION   2500    // E acceleration for retracts
+#define DEFAULT_RETRACT_ACCELERATION   5000    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION    2500    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
