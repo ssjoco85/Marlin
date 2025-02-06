843a86c3db7c7a506fb30d9a188a74bf3a59a213
ssjoco85 <122283808+ssjoco85@users.noreply.github.com>
2025-02-06 22:08:17 +0100

    acc3

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1336,9 +1336,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION           1200    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_ACCELERATION           1500    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION   5000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION    2500    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_TRAVEL_ACCELERATION    2000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk limits (mm/s)
